9a96fde5c8cf02dc0eb67e781d5c5dc91946d85d
nekomehako <ngnlkkngsm@yahoo.co.jp>
2022-01-30 11:52:02 +0900

    有限状態機械の追加

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,29 @@
 window.addEventListener("load", main);
+
+export type State = "Title" | "Battle" | "Result" | "End";
+
 function main() {
-  console.log("Hello world.");
+  //状態遷移によるシーン管理
+
+  let currentState: State = "Title";
+  stateMachine: for (;;) {
+    switch (currentState) {
+      case "Title":
+        console.log(currentState);
+        currentState = "Battle";
+        break;
+      case "Battle":
+        console.log(currentState);
+        currentState = "Result";
+        break;
+      case "Result":
+        console.log(currentState);
+        currentState = "End";
+        break;
+      case "End":
+        console.log(currentState);
+        break stateMachine;
+    }
+  }
+  console.log("end");
 }
