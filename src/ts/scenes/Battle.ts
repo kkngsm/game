7473d9469e342855349fe9ca262d89981674e175
nekomehako <ngnlkkngsm@yahoo.co.jp>
2022-02-01 11:26:07 +0900

    操作を追加

--- src/ts/scenes/Battle.ts
+++ src/ts/scenes/Battle.ts
@@ -10,6 +10,7 @@ import { Scene, SceneProps, State } from "./Scene";
 export default class Battle extends Scene {
   private scene: TScene;
   private camera: PerspectiveCamera;
+  private cube: Mesh;
   constructor(prop: SceneProps) {
     super(prop);
     this.scene = new TScene();
@@ -21,44 +22,51 @@ export default class Battle extends Scene {
       10000
     );
     this.camera.position.z = 100;
-    this.camera.position.y = 50;
     this.camera.lookAt(new Vector3(0, 0, 0));
 
     const box = new BoxGeometry(10, 10, 10);
     const mat = new MeshBasicMaterial({ color: 0x1ec876 });
-    const cube = new Mesh(box, mat);
-    cube.position.set(0, 0, 0);
-    this.scene.add(cube);
+    this.cube = new Mesh(box, mat);
+    this.cube.position.set(0, 0, 0);
+    this.scene.add(this.cube);
   }
 
   async run(): Promise<State> {
-    console.log("Battle");
     return new Promise<void>((resolve) => {
       const loop = (time: number) => {
+        this.operation();
         this.draw(time);
         if (!this.key.enter) {
-          requestAnimationFrame((time) => {
-            loop(time);
-          });
+          requestAnimationFrame(loop);
         } else {
           resolve();
         }
       };
-      requestAnimationFrame((time) => {
-        loop(time);
-      });
+      requestAnimationFrame(loop);
     }).then(() => {
       return "Result" as State;
     });
   }
 
   private draw(time: number): void {
-    this.camera.position.x = Math.sin(time * 0.001) * 100;
-    this.camera.position.z = Math.cos(time * 0.001) * 100;
     this.camera.lookAt(new Vector3(0, 0, 0));
-
     this.renderer.setRenderTarget(null);
     this.renderer.clear();
     this.renderer.render(this.scene, this.camera);
+  }
+
+  private operation() {
+    if (this.key.w) {
+      this.cube.position.y = this.cube.position.y + 0.5;
+    }
+    if (this.key.a) {
+      this.cube.position.x = this.cube.position.x - 0.5;
+    }
+    if (this.key.s) {
+      this.cube.position.y = this.cube.position.y - 0.5;
+    }
+    if (this.key.d) {
+      this.cube.position.x = this.cube.position.x + 0.5;
+    }
   }
 }
